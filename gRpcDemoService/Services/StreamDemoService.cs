6e0259b502ab74ca1a3fb5e0eb112f3b8874432a
Rely Ky <relyky@hotmail.com>
2023-12-13 18:46:19 +0800

    BidirectionStreamDemo

--- gRpcDemoService/Services/StreamDemoService.cs
+++ gRpcDemoService/Services/StreamDemoService.cs
@@ -18,12 +18,13 @@ public class StreamDemoService : StreamDemo.StreamDemoBase
   {
     for (int i = 1; i <= 20; i++)
     {
+      // 模擬處理時間
+      var delayTime = _random.Next(1, 2000);
+      await Task.Delay(delayTime);
+
       string message = $"Message {i}";
       await replyStream.WriteAsync(new Test { Message = message });
       _logger.LogInformation($"已回應：{message}");
-
-      var delayTime = _random.Next(1, 2000);
-      await Task.Delay(delayTime);
     }
   }
 
@@ -39,5 +40,33 @@ public class StreamDemoService : StreamDemo.StreamDemoBase
 
     _logger.LogInformation($"Client Streaming Completed.");
     return new Test { Message = $"茲收到訊息{receiveCount}條。" };
+  }
+
+  public override async Task BidirectionStreamDemo(IAsyncStreamReader<Test> reqStream, IServerStreamWriter<Test> replyStream, ServerCallContext ctx)
+  {
+    var taskList = new List<Task>();
+
+    while(await reqStream.MoveNext())
+    {
+      var reqMsg = reqStream.Current;
+      _logger.LogInformation($"已收到：{reqMsg.Message}");
+
+      //## 再開執行緒平行處理上傳訊息
+      var msgTask = Task.Run(async () =>
+      {
+        // 模擬處理時間
+        var delayTime = _random.Next(1, 2000);
+        await Task.Delay(delayTime);
+
+        var replyMsg = new Test { Message = $"回應訊息 → {reqMsg.Message}" };
+        await replyStream.WriteAsync(replyMsg);
+        _logger.LogInformation($"已回應：{replyMsg.Message}");
+      });
+
+      taskList.Add(msgTask);
+    }
+
+    await Task.WhenAll(taskList);
+    _logger.LogInformation("Bidirectional Streaming Completed.");
   }
 }
