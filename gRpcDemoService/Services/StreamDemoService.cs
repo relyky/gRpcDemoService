56e431c316b27da25597b1721190d608d53b758c
Rely Ky <relyky@hotmail.com>
2023-12-13 17:24:34 +0800

    Serveer Stream

---
+++ gRpcDemoService/Services/StreamDemoService.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using GrpcDemoService;
+
+namespace GrpcDemoService.Services;
+
+public class StreamDemoService : StreamDemo.StreamDemoBase
+{
+  readonly ILogger<StreamDemoService> _logger;
+  Random _random;
+
+  public StreamDemoService(ILogger<StreamDemoService> logger)
+  {
+    _logger = logger;
+    _random = new Random();
+  }
+
+  public override async Task ServerStreamDemo(Test req, IServerStreamWriter<Test> replyStream, ServerCallContext ctx)
+  {
+    for (int i = 1; i <= 20; i++)
+    {
+      string message = $"Message {i}";
+      await replyStream.WriteAsync(new Test { Message = message });
+      _logger.LogInformation($"已回應：{message}");
+
+      var delayTime = _random.Next(1, 2000);
+      await Task.Delay(delayTime);
+    }
+  }
+}
